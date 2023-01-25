4af5f89ab69a0fc1ea50a69bca5610e04f4c1f2e
paulro1575 <paulro1575@gmail.com>
2023-01-25 09:08:46 -0500

    010-log-user-id-into-microservices

--- microservice2/src/main/kotlin/com/security/micro2/service/ProtectedResourceService.kt
+++ microservice2/src/main/kotlin/com/security/micro2/service/ProtectedResourceService.kt
@@ -5,6 +5,7 @@ import com.security.micro2.utils.ADMIN_ROLE
 import com.security.micro2.utils.CUSTOMER_ROLE
 import org.apache.log4j.Logger
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.oauth2.core.DefaultOAuth2AuthenticatedPrincipal
 import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthentication
 import org.springframework.stereotype.Service
 
@@ -18,8 +19,10 @@ class ProtectedResourceService: IProtectedResourceService {
 
     override fun getProtectedResource(auth: BearerTokenAuthentication): ProtectedResource {
         val name = microserviceName ?: "Default Microservice"
+        val principal: DefaultOAuth2AuthenticatedPrincipal = auth.principal as DefaultOAuth2AuthenticatedPrincipal
         logger.info("MICROSERVICE NAME: $name, TOKEN-TYPE: ${auth.token.tokenType}")
         logger.info("TOKEN-VALUE: ${auth.token.tokenValue}")
+        logger.info("USER ID: ${principal.name}")
         
         val isAdmin = auth.authorities.any{it == ADMIN_ROLE}
         val isCustomer = auth.authorities.any{it == CUSTOMER_ROLE}
